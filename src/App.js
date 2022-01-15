bd4ea8c4b886f442a781698b44e746df9be08aed
Muzammil Jumshaid <thisismjey@gmail.com>
2022-01-15 17:40:51 +0500

    Final code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { useState, useEffect }  from 'react';
+const Home = () => {
+const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [coins, setCoins] = useState([]);
+    useEffect(() => {
+        fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=1000&page=1&sparkline=false")
+            .then(res => res.json())
+            .then(
+                (data) => {
+                    setIsLoaded(true);
+                    setCoins(data);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+      }, [])
+if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    } else {
+        return (
+          <>
+            <div className="container">
+              {coins.map(coin => (
+                  <div key={coin.id} className="card">
+                      <img src={coin.image} alt={coin.name} />
+                      <h3>{coin.market_cap_change_percentage_24h}%</h3>
+                      <p>{coin.name} <span>{coin.symbol}</span></p>
+                      <button className="delegate">Delegate</button>
+                  </div>
+                ))}
+            </div>
+          </>
+        );
+    }
 }
-
-export default App;
+export default Home;
